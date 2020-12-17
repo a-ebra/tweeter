352cec98896d083952787bcf69b1f92523f84aaa
Aqil Ebrahim <aqilebrahim@gmail.com>
2020-12-17 11:47:15 +0000

    init commit, CreateTweetElement, redner fx, helper fx, load fx

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,66 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// escape helper fx
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+//generate dom structure for tweet from given obj
+const createTweetElement = function(tweet) {
+  const time = moment(tweet.created_at).fromNow();
+  const body = escape(tweet.content.text);
+  const handle = tweet.user.handle;
+  const user = tweet.user.name;
+  const profilePic = tweet.user.avatars;
+  
+  const $tweet = $(`
+  <article class="tweet">
+  <header>
+  <div class="user">
+    <img src=${profilePic} class="profile-pic">
+    <p>${user}</p>
+  </div>
+  <div>
+    <p class="user">${handle}</p>
+  </div>
+  </header>
+
+  <div class="text-body">
+    <p>${body}</p>
+  </div>
+
+  <footer>
+    <p>${time}</p>
+    <p class="icons">
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i> 
+    <i class="fas fa-heart"></i>
+    </p>
+  </footer>
+  </article>
+`);
+return $tweet;
+};
+
+// fx to append tweet objects to tweets container
+const renderTweets = function(tweets) {
+  $('#tweets').empty();
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets').prepend($tweet)
+  }
+};
+
+// GET tweets from /tweets
+const loadTweets = function () {
+  $.ajax({method: 'GET', url: '/tweets'})
+  .then((data) => {renderTweets(data)})
+};
+
+loadTweets();
+
+
+
