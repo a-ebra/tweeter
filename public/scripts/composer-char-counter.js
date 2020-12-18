090c0371d73285c648a574846e4aa0b7cc44b30d
Aqil Ebrahim <aqilebrahim@gmail.com>
2020-12-18 00:46:05 +0000

    removed console log statements.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
 
   $("#tweet-text").on("keyup", function(event) {
-    console.log('hello')
     const $maxChars = 140;
-    console.log($maxChars)
     let $counter = $(".counter");
     let $currentChars = $(this).val().length;
     let $remainingChars = $maxChars - $currentChars;
-    console.log($remainingChars)
     $counter.val($remainingChars)
     if ($remainingChars <= 0) {
       $counter.css("color", "red")
